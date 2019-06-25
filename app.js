d38642b3ccd00501487edcdea508f93d85668f2b
Bhagyashri-Zerkunte <bsz50036@gmail.com>
2019-06-25 14:42:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var http=require('http');   // module
+//object----- model
+var product ={
+    title:"Lily",
+    description:"Valentine Flower",
+    unitprice:25,
+    likes:500,
+    quantity:3000
+};
+
+var count=45;
+count++;
+var controller=function(request,response)
+{
+	response.writeHead(200,
+	                {"Content-Type" : "text/plain"});
+	                response.end( JSON.stringify(product));
+};
+var server=http.createServer(controller);
+server.listen(7000);
+console.log("listening on port 7000");
